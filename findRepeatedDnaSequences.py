4420a69fba37b4057c6413d57affb2aac03f4dde
Joana Vukatana <vukatana.j@husky.neu.edu>
2022-05-29 19:27:46 -0400

    3 done

--- findRepeatedDnaSequences.py
+++ findRepeatedDnaSequences.py
@@ -3,9 +3,15 @@ import unittest
 
 
 def findRepeatedDnaSequences(dnaSeq: str) -> List[str]:
+    """Iterate through every position in the string, except for the last 9
+    because we want at least 9 characters after pointer i to make a 
+    window of 10 characters.
+    """
     temp, results = set(), set()
     for i in range(len(dnaSeq) - 9):
+        # +10 since the second index is non-inclusive in python
         substring = dnaSeq[i:i+10]
+        # if we don't see the substring in temp, we add it
         if substring not in temp:
             temp.add(substring)
         elif substring not in results:
@@ -22,6 +28,10 @@ class TestProblems(unittest.TestCase):
         actual_1 = findRepeatedDnaSequences("AAAAAAAAAAAAA")
         expected_1 = ["AAAAAAAAAA"]
         self.assertCountEqual(actual_1, expected_1)
+
+        actual_2 = findRepeatedDnaSequences("ACGTACGTACTTTTACGTACGTAC")
+        expected_2 = ["ACGTACGTAC"]
+        self.assertCountEqual(actual_2, expected_2)
     
 if __name__ == '__main__':
     unittest.main()
---
+++ validPalindrome.py
@@ -0,0 +1,50 @@
+"""
+A phrase is a palindrome if, after converting all uppercase letters into lowercase letters and removing all non-alphanumeric characters, it reads the same forward and backward. Alphanumeric characters include letters and numbers.
+
+Given a string s, return true if it is a palindrome, or false otherwise.
+
+Example 1:
+
+Input: s = "A man, a plan, a canal: Panama"
+Output: true
+Explanation: "amanaplanacanalpanama" is a palindrome.
+
+Example 2:
+
+Input: s = "race a car"
+Output: false
+Explanation: "raceacar" is not a palindrome.
+
+Example 3:
+
+Input: s = " "
+Output: true
+Explanation: s is an empty string "" after removing non-alphanumeric characters.
+Since an empty string reads the same forward and backward, it is a palindrome.
+
+"""
+
+import unittest
+
+
+def isPalindrome(s: str) -> bool:
+    new_string = (''.join(e for e in s if e.isalnum())).lower()
+    # Print the original string in reverse order if we set the stride to -1:
+    reversed_string = new_string[::-1]
+    return new_string == reversed_string
+
+class TestProblems(unittest.TestCase):
+
+    def test_find_repeated_dna_sequences(self):
+        actual = isPalindrome("A man, a plan, a canal: Panama")
+        self.assertTrue(actual)
+
+        actual_1 = isPalindrome("race a car")
+        self.assertFalse(actual_1)
+
+        actual_2 = isPalindrome(" ")
+        self.assertTrue(actual_2)
+
+    
+if __name__ == '__main__':
+    unittest.main()
